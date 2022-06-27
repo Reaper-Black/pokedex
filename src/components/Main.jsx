6f98ee6a6b5f3322ad4d69fe8e7af2796d15d7e6
Andres Chacon <andres_men98@hotmail.com>
2022-06-27 01:56:22 -0500

    ci: Adding a styles

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <>
+        <div className="container">
+            <div className="left-content">    
+
+            </div>
+            <div className="right-content">
+
+            </div>
+        </div>
+        
+        
+        </> 
+
+
+    )
+}
+
+export default Main;
