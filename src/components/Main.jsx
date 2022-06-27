3c14dd20020f6bfbbd94bcfa46224129d11b4bee
Andres Chacon <andres_men98@hotmail.com>
2022-06-27 03:49:35 -0500

    feat: finalize the Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,60 +1,63 @@
 import React from "react";
-import Card from "./Card"
+import Card from "./Card";
 import Pokeinfo from "./Pokeinfo";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Main = () => {
-    const [pokeData, setPokeData]=useState([]);
-    const [loading, setLoading]=useState(true);
-    const [url, setUrl]=useState("https://pokeapi.co/api/v2/pokemon/");
-    const [nextUrl, setNextUrl]=useState();
-    const [prevUrl, setPrevUrl]=useState();
+const Main=()=>{
+    const [pokeData,setPokeData]=useState([]);
+    const [loading,setLoading]=useState(true);
+    const [url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/")
+    const [nextUrl,setNextUrl]=useState();
+    const [prevUrl,setPrevUrl]=useState();
+    const [pokeDex,setPokeDex]=useState();
 
-    const pokeFun = async() => {
+    const pokeFun=async()=>{
         setLoading(true)
         const res=await axios.get(url);
-        //console.log(res.data.results);
         setNextUrl(res.data.next);
         setPrevUrl(res.data.previous);
-        getPokemon(res.data.results);
-        setLoading(false);
-        //console.log(pokeData);
+        getPokemon(res.data.results)
+        setLoading(false)
     }
-
     const getPokemon=async(res)=>{
-        res.map(async(item)=>{
-            const result=await axios.get(item.url);
-            //console.log(result.data)
-            setPokeData(state=>{
-                state=[...state,result.data];
-                return state;
-            })
-        })
+       res.map(async(item)=>{
+          const result=await axios.get(item.url)
+          setPokeData(state=>{
+              state=[...state,result.data]
+              state.sort((a,b)=>a.id>b.id?1:-1)
+              return state;
+          })
+       })   
     }
-
-    useEffect(() => {
+    useEffect(()=>{
         pokeFun();
     },[url])
-    
     return(
         <>
-        <div className="container">
-            <div className="left-content">    
-            <Card pokemon={pokeData}  loading={loading}/>
-            <div className="btn-group">
-                <button>Previous</button>
-                <button>Next</button>
-            </div>
-            </div>
-            <div className="right-content">
-                <Pokeinfo/>
-            </div>
-        </div>
-        
-        </> 
+            <div className="container">
+                <div className="left-content">
+                    <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>
+                    
+                    <div className="btn-group">
+                        {  prevUrl && <button onClick={()=>{
+                            setPokeData([])
+                           setUrl(prevUrl) 
+                        }}>Previous</button>}
 
+                        { nextUrl && <button onClick={()=>{
+                            setPokeData([])
+                            setUrl(nextUrl)
+                        }}>Next</button>}
+
+                    </div>
+                </div>
+                <div className="right-content">
+                   <Pokeinfo data={pokeDex}/>
+                </div>
+            </div>
+        </>
     )
 }
-
 export default Main;
